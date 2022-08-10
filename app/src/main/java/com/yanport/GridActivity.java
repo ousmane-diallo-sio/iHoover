80cc8346fabfce718d7fd46e1cd399134c32bb15
DIALLO <ousmane.diallo1.sio@gmail.com>
2022-08-10 20:34:07 +0200

    10/08/2022

---
+++ app/src/main/java/com/yanport/GridActivity.java
@@ -0,0 +1,59 @@
+package com.yanport;
+
+import static com.yanport.R.color.white;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+
+public class GridActivity extends AppCompatActivity {
+
+    String logTag = "GridActivity";
+
+    int gridHeight;
+    int gridLength;
+
+    TableLayout gridContainer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grid);
+
+        this.gridContainer = findViewById(R.id.gridContainer);
+
+        Bundle bundle = new Bundle();
+        bundle = getIntent().getExtras();
+
+        gridHeight = bundle.getInt("gridHeight");
+        gridLength = bundle.getInt("gridLength");
+
+        Log.i(this.logTag, String.format("Grid height : %s, Grid Length : %s", gridHeight, gridLength));
+
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
+        for(int i = 0; i < this.gridHeight; i++){
+            LinearLayout row = (LinearLayout) inflater.inflate(R.layout.grid_row, null);
+            for(int j = 0; j < this.gridLength; j++){
+                View box = inflater.inflate(R.layout.grid_box, row);
+            }
+
+            gridContainer.addView(row);
+        }
+
+    }
+
+    public void quitActivity(View view) {
+        Intent intent = new Intent(GridActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/yanport/MainActivity.java
@@ -0,0 +1,53 @@
+package com.yanport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    String logTag = "MainActivity";
+    Spinner spHeight;
+    Spinner spLength;
+    List possibleValues = new ArrayList();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.spHeight = findViewById(R.id.spHeight);
+        this.spLength = findViewById(R.id.spLength);
+
+        for(int i=2; i<11; i++){
+            possibleValues.add(i);
+            Log.i(this.logTag, String.format("Ajout de l'élement n°%s à la liste", i));
+        }
+
+        ArrayAdapter<Integer> spsAdapter = new ArrayAdapter<Integer>(
+                this,
+                android.R.layout.simple_spinner_dropdown_item,
+                possibleValues
+        );
+
+        spHeight.setAdapter(spsAdapter);
+        spLength.setAdapter(spsAdapter);
+
+    }
+
+    public void createGrid(View view) {
+        Intent intent = new Intent(MainActivity.this, GridActivity.class);
+        intent.putExtra("gridHeight", (Integer) spHeight.getSelectedItem());
+        intent.putExtra("gridLength", (Integer) this.spLength.getSelectedItem());
+
+        startActivity(intent);
+    }
+}
